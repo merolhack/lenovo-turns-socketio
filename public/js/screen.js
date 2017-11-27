df0f6a7bf8028dbb5755ef2b5643ac0018fd6ee7
Lenin Meza <merolhack@gmail.com>
2017-11-27 07:18:20 -0600

    :sparkles: feat: SweetAlert for request IP

--- public/js/screen.js
+++ public/js/screen.js
@@ -1,29 +1,42 @@
-(function(io, $) {
+(function(io, $, swal) {
     'use strict';
 
-    const url = 'http://127.0.0.1:80';
-    const options = {
-        path: '/turns'
-    };
-    const socket = io(url, options);
-    // Define functions
-    function getCurrentTurn(cb) {
-        socket.emit('get-turn', {});
-        socket.on('current-turn', (payload) => cb(payload));
-    }
-    function subscribeToCurrentTurn(cb) {
-        socket.on('turn-created', (payload) => cb(null, payload));
-    }
-    // Use callback with functions
-    getCurrentTurn(function(payload) {
-        console.log('payload:', JSON.stringify(payload));
-        $('div.turno-activo span.turno-activo-codigo').text(payload.group + '' + payload.counter);
-        $('div.turno-activo span.turno-activo-modulo span').text(payload.wind0w);
-        var sound = document.getElementById("audio");
-        sound.play();
+    setTimeout(function() {
+        $('.swal-content__input' ).val('127.0.0.1');
+    }, 300);
+    swal({
+        text: 'Ingrese la IP del panel de control:',
+        content: "input",
+        button: {
+            text: "Guardar",
+            closeModal: true,
+        },
+    })
+    .then(function(ip) {
+        const url = 'http://'+ ip +':80';
+        const options = {
+            path: '/turns'
+        };
+        const socket = io(url, options);
+        // Define functions
+        function getCurrentTurn(cb) {
+            socket.emit('get-turn', {});
+            socket.on('current-turn', (payload) => cb(payload));
+        }
+        function subscribeToCurrentTurn(cb) {
+            socket.on('turn-created', (payload) => cb(null, payload));
+        }
+        // Use callback with functions
+        getCurrentTurn(function(payload) {
+            console.log('payload:', JSON.stringify(payload));
+            $('div.turno-activo span.turno-activo-codigo').text(payload.group + '' + payload.counter);
+            $('div.turno-activo span.turno-activo-modulo span').text(payload.wind0w);
+            var sound = document.getElementById("audio");
+            sound.play();
+        });
+        subscribeToCurrentTurn(function(err, payload) {
+            console.log('currentTurn:', payload);
+            $('div.turno-siguiente span.codigo').text(payload.groupName + '' + payload.counter);
+        });
     });
-    subscribeToCurrentTurn(function(err, payload) {
-        console.log('currentTurn:', payload);
-        $('div.turno-siguiente span.codigo').text(payload.groupName + '' + payload.counter);
-    });
-})(window.io, window.jQuery);
+})(window.io, window.jQuery, window.swal);
